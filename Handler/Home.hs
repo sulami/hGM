2efa98a2ad8c0149dd4f1b8bb805718e6953cf7f
Robin Schroer <sulami@peerwire.org>
2015-08-23 00:29:00 +0200

    remove the standard form

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -1,8 +1,6 @@
 module Handler.Home where
 
 import Import
-import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3,
-                              withSmallInput)
 
 getHomeR :: Handler Html
 getHomeR = do
@@ -10,7 +8,3 @@ getHomeR = do
         setTitle "Home"
         $(widgetFile "homepage")
 
-sampleForm :: Form (FileInfo, Text)
-sampleForm = renderBootstrap3 BootstrapBasicForm $ (,)
-    <$> fileAFormReq "Choose a file"
-    <*> areq textField (withSmallInput "What's on the file?") Nothing
