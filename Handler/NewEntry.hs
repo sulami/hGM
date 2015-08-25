448bb42ece2ca8fa4232e3dc401003439eb90810
Robin Schroer <sulami@peerwire.org>
2015-08-25 15:11:18 +0200

    add the newentry page

--- Handler/NewEntry.hs
+++ Handler/NewEntry.hs
@@ -1,24 +1,19 @@
-module Handler.Entry where
+module Handler.NewEntry where
 
 import Import
-
-import Data.Maybe (fromJust)
 
 entryForm :: Form Entry
 entryForm = renderDivs $ Entry
   <$> areq textField "Title"    Nothing
   <*> areq textField "Content"  Nothing
 
-getEntryR :: EntryId -> Handler Html
-getEntryR entryId = do
-  entry <- runDB $ get404 entryId
+getNewEntryR :: Handler Html
+getNewEntryR = do
   (entryWidget, enctype) <- generateFormPost entryForm
-  defaultLayout $ do
-    setTitle $ toHtml $ entryName $ entry
-    $(widgetFile "entry")
+  defaultLayout $(widgetFile "newentry")
 
-postEntryR :: EntryId -> Handler Html
-postEntryR entryId = do
+postNewEntryR :: Handler Html
+postNewEntryR = do
   ((res,entryWidget), enctype) <- runFormPost entryForm
   case res of
     FormSuccess entry -> do
---
+++ test/Handler/NewEntrySpec.hs
@@ -0,0 +1,14 @@
+module Handler.NewEntrySpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getNewEntryR" $ do
+        error "Spec not implemented: getNewEntryR"
+
+
+    describe "postNewEntryR" $ do
+        error "Spec not implemented: postNewEntryR"
+
