515b36cbc3cac057ec0be9d2bf3202e4f43b8242
Robin Schroer <sulami@peerwire.org>
2015-08-25 15:51:57 +0200

    implement modifying existing entries

--- Handler/Entry.hs
+++ Handler/Entry.hs
@@ -2,16 +2,20 @@ module Handler.Entry where
 
 import Import
 
-import Data.Maybe (fromJust)
-
 entryForm :: Form Entry
 entryForm = renderTable $ Entry
   <$> areq textField "Title" Nothing
   <*> areq textareaField "Content" Nothing
 
+prepEntryForm :: Entry -> Form Entry
+prepEntryForm entry = renderTable $ Entry
+  <$> areq textField "Title" (Just $ entryName entry)
+  <*> areq textareaField "Content" (Just $ entryContent entry)
+
 getEntryR :: EntryId -> Handler Html
 getEntryR entryId = do
   entry <- runDB $ get404 entryId
+  (entryWidget, enctype) <- generateFormPost $ prepEntryForm entry
   defaultLayout $ do
     setTitle $ toHtml $ entryName $ entry
     $(widgetFile "entry")
@@ -19,9 +23,10 @@ getEntryR entryId = do
 postEntryR :: EntryId -> Handler Html
 postEntryR entryId = do
   ((res,entryWidget), enctype) <- runFormPost entryForm
+  entry <- runDB $ get404 entryId
   case res of
-    FormSuccess entry -> do
-      entryId <- runDB $ insert entry
+    FormSuccess entryData -> do
+      runDB $ replace entryId entryData
       setMessage $ toHtml $ (entryName entry) <> " created"
       redirect $ EntryR entryId
     _ -> defaultLayout $ do
