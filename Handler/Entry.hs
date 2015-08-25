8cc082e764d2917a57827fb0f633e36c67b0b57d
Robin Schroer <sulami@peerwire.org>
2015-08-25 16:16:40 +0200

    add some style fluff

--- Handler/Entry.hs
+++ Handler/Entry.hs
@@ -3,12 +3,12 @@ module Handler.Entry where
 import Import
 
 entryForm :: Form Entry
-entryForm = renderTable $ Entry
+entryForm = renderSemantic $ Entry
   <$> areq textField "Title" Nothing
   <*> areq textareaField "Content" Nothing
 
 prepEntryForm :: Entry -> Form Entry
-prepEntryForm entry = renderTable $ Entry
+prepEntryForm entry = renderSemantic $ Entry
   <$> areq textField "Title" (Just $ entryName entry)
   <*> areq textareaField "Content" (Just $ entryContent entry)
 
@@ -18,6 +18,7 @@ getEntryR entryId = do
   (entryWidget, enctype) <- generateFormPost $ prepEntryForm entry
   defaultLayout $ do
     setTitle $ toHtml $ entryName $ entry
+    addStylesheetRemote "//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.0.8/semantic.min.css"
     $(widgetFile "entry")
 
 postEntryR :: EntryId -> Handler Html
@@ -32,4 +33,22 @@ postEntryR entryId = do
     _ -> defaultLayout $ do
       setTitle "Bad."
       $(widgetFile "error")
+
+renderSemantic :: Monad m => FormRender m a
+renderSemantic aform fragment = do
+    (res, views') <- aFormToForm aform
+    let views = views' []
+    let widget = [whamlet|
+$newline never
+\#{fragment}
+$forall view <- views
+    <div .ui .field :fvRequired view:.required :not $ fvRequired view:.optional>
+        <label for=#{fvId view}>#{fvLabel view}
+        $maybe tt <- fvTooltip view
+            <div .tooltip>#{tt}
+        ^{fvInput view}
+        $maybe err <- fvErrors view
+            <div .errors>#{err}
+|]
+    return (res, widget)
 
