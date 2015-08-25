de977529e5f47c3a7228ab1b2d63fb35c922e8fa
Robin Schroer <sulami@peerwire.org>
2015-08-25 15:03:22 +0200

    do something with adding new and displaying entries

--- Handler/Entry.hs
+++ Handler/Entry.hs
@@ -2,8 +2,34 @@ module Handler.Entry where
 
 import Import
 
+import Data.Maybe (fromJust)
+
+entryForm :: Form Entry
+entryForm = renderDivs $ Entry
+  <$> areq textField "Title"    Nothing
+  <*> areq textField "Content"  Nothing
+
 getEntryR :: EntryId -> Handler Html
-getEntryR = error "Not yet implemented: getEntryR"
+getEntryR entryId = do
+  mbEntry <- runDB $ get entryId
+  (entryWidget, enctype) <- generateFormPost entryForm
+  case mbEntry of
+    Nothing -> defaultLayout $ do
+                  $(widgetFile "newentry")
+    _ -> defaultLayout $ do
+            let entry = fromJust mbEntry
+            setTitle $ toHtml $ entryName $ entry
+            $(widgetFile "entry")
 
 postEntryR :: EntryId -> Handler Html
-postEntryR = error "Not yet implemented: postEntryR"
+postEntryR entryId = do
+  ((res,entryWidget), enctype) <- runFormPost entryForm
+  case res of
+    FormSuccess entry -> do
+      entryId <- runDB $ insert entry
+      setMessage $ toHtml $ (entryName entry) <> " created"
+      redirect $ EntryR entryId
+    _ -> defaultLayout $ do
+      setTitle "Bad."
+      $(widgetFile "error")
+
