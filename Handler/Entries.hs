7f70579be4d2d1e6d0c0f4f81c0e589393a01db7
Robin Schroer <sulami@peerwire.org>
2015-08-25 16:35:01 +0200

    add a list of existing entries

---
+++ Handler/Entries.hs
@@ -0,0 +1,8 @@
+module Handler.Entries where
+
+import Import
+
+getEntriesR :: Handler Html
+getEntriesR = do
+  entries <- runDB $ selectList [] [Asc EntryId, LimitTo 5]
+  defaultLayout $(widgetFile "entries")
---
+++ test/Handler/EntriesSpec.hs
@@ -0,0 +1,10 @@
+module Handler.EntriesSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getEntriesR" $ do
+        error "Spec not implemented: getEntriesR"
+
