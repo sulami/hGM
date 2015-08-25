4c76f83dcb820ae40ef53ccb5b4a0d5165567613
Robin Schroer <sulami@peerwire.org>
2015-08-25 14:02:45 +0200

    add prototype entry handler/model

---
+++ Handler/Entry.hs
@@ -0,0 +1,9 @@
+module Handler.Entry where
+
+import Import
+
+getEntryR :: EntryId -> Handler Html
+getEntryR = error "Not yet implemented: getEntryR"
+
+postEntryR :: EntryId -> Handler Html
+postEntryR = error "Not yet implemented: postEntryR"
---
+++ test/Handler/EntrySpec.hs
@@ -0,0 +1,14 @@
+module Handler.EntrySpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getEntryR" $ do
+        error "Spec not implemented: getEntryR"
+
+
+    describe "postEntryR" $ do
+        error "Spec not implemented: postEntryR"
+
