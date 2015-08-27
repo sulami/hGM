44488d44555e83f494a76b74e96e1a267dca512c
Robin Schroer <sulami@peerwire.org>
2015-08-27 14:03:12 +0200

    add a global rng to the application

--- Application.hs
+++ Application.hs
@@ -39,6 +39,8 @@ import Handler.EntryEdit
 import Handler.EntryDel
 import Handler.Dice
 
+import Game.DnD.GM.Dice (initializeIO)
+
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
 -- comments there for more details.
@@ -57,6 +59,7 @@ makeFoundation appSettings = do
     appStatic <-
         (if appMutableStatic appSettings then staticDevel else static)
         (appStaticDir appSettings)
+    rng <- initializeIO
 
     -- We need a log function to create a connection pool. We need a connection
     -- pool to create our foundation. And we need our foundation to get a
--- Foundation.hs
+++ Foundation.hs
@@ -9,6 +9,8 @@ import Yesod.Default.Util   (addStaticContentExternal)
 import Yesod.Core.Types     (Logger)
 import qualified Yesod.Core.Unsafe as Unsafe
 
+import System.Random (StdGen)
+
 -- | The foundation datatype for your application. This can be a good place to
 -- keep settings and values requiring initialization before your application
 -- starts running, such as database connections. Every handler will have
@@ -19,6 +21,7 @@ data App = App
     , appConnPool    :: ConnectionPool -- ^ Database connection pool.
     , appHttpManager :: Manager
     , appLogger      :: Logger
+    , rng            :: StdGen
     }
 
 instance HasHttpManager App where
