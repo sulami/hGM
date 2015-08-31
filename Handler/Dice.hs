3ae884773fba0fad42aa1191f4db51d3951016c8
Robin Schroer <sulami@peerwire.org>
2015-08-31 14:32:21 +0200

    streamline the dice rolling page

--- Handler/Dice.hs
+++ Handler/Dice.hs
@@ -7,7 +7,7 @@ getDiceR = do
   _ <- requireAuthId
   defaultLayout $ do
     setTitle "Dice"
-    let dice = [20,12,10,8,6,4] :: [Int]
+    let dice = [100,20,12,10,8,6,4] :: [Int]
     let numDice = [1..10] :: [Int]
     $(widgetFile "dice")
 
