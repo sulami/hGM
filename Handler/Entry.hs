84b47330d36d18623fef81218e61b380f72bc533
Robin Schroer <sulami@peerwire.org>
2015-09-15 15:27:12 +0200

    add inThis and thisIn data to new entries

--- Handler/Entry.hs
+++ Handler/Entry.hs
@@ -7,6 +7,8 @@ getEntryR entryId = do
   user <- requireAuthId
   entry <- runDB $ get404 entryId
   camp <- runDB . get404 $ entryCampaignId entry
+  inThis <- runDB $ selectList [EntryId <-. entryInThis entry] []
+  thisIn <- runDB $ selectList [EntryId <-. entryThisIn entry] []
   if user /= campaignOwnerId camp
     then do
       setMessage "Permission denied."
--- Handler/EntryNew.hs
+++ Handler/EntryNew.hs
@@ -22,31 +22,36 @@ postEntryNewR cid = do
   case res of
     FormSuccess entry -> do
       otherEntries' <- runDB $ selectList [EntryCampaignId ==. cid] []
-      let otherEntries = map (\(Entity _ e) -> e) otherEntries'
+      let otherEntries = map entityToTuple otherEntries'
           inThis = filter (inEntry entry) otherEntries
           thisIn = filter (entryIn entry) otherEntries
-      -- TODO add these two above to the entry before saving it
       -- TODO modify the other affected entries as well
       -- TODO add this to entryEdit
+      -- FIXME make this a single DB query
       entryId <- runDB $ insert entry
+      runDB $ update entryId [ EntryInThis =. map fst inThis
+                             , EntryThisIn =. map fst thisIn ]
       setMessage . toHtml $ entryName entry <> " created"
       redirect . EntriesR $ EntryR entryId
     _ -> defaultLayout $ do
       setMessage "Error creating entry."
       $(widgetFile "error")
 
+entityToTuple :: Entity a -> (Key a, a)
+entityToTuple (Entity k e) = (k, e)
+
 unmarkdown :: MD.Markdown -> Text
 unmarkdown = TL.toStrict . (\(MD.Markdown e) -> e)
 
 -- Is an entry referenced in this one?
-inEntry :: Entry -> Entry -> Bool
+inEntry :: Entry -> (Key Entry, Entry) -> Bool
 inEntry e = (`textMatch` TS.words (unmarkdown $ entryContent e)) . TS.words .
-              entryName
+              entryName . snd
 
 -- Is this entry referenced in another one?
-entryIn :: Entry -> Entry -> Bool
+entryIn :: Entry -> (Key Entry, Entry) -> Bool
 entryIn e = textMatch (TS.words $ entryName e) . TS.words . unmarkdown .
-              entryContent
+              entryContent . snd
 
 -- Match multi-word names.
 textMatch :: [Text] -> [Text] -> Bool
