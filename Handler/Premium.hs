980e384fc526408b8bcb8340024b9155b296eb6c
Robin Schroer <sulami@peerwire.org>
2015-09-25 11:38:01 +0200

    prototype the premium get page

--- Handler/Premium.hs
+++ Handler/Premium.hs
@@ -3,7 +3,13 @@ module Handler.Premium where
 import Import
 
 getPremiumR :: Handler Html
-getPremiumR = error "Not yet implemented: getPremiumR"
+getPremiumR = do
+  Entity uid user <- requireAuth
+  when (userPremium user) . redirect $ AccountR OverviewR
+  defaultLayout $ do
+    setTitle "Premium"
+    $(widgetFile "premium")
 
 postPremiumR :: Handler Html
 postPremiumR = error "Not yet implemented: postPremiumR"
+
