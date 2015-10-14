94fc3ecca3a63728f9e431db59481f859df0036f
Robin Schroer <sulami@peerwire.org>
2015-10-14 17:14:03 +0200

    add a addpremium helper function

--- Import/Premium.hs
+++ Import/Premium.hs
@@ -1,9 +1,24 @@
 module Import.Premium where
 
 import Import
+import Data.Maybe (fromJust)
+import Data.Time.Calendar (addDays)
 
+-- | Check if a user has active premium that expires sometime in the future.
 hasPremium :: User -> IO Bool
 hasPremium user = do
   today <- fmap utctDay getCurrentTime
   return $ userPremium user && userPremiumUntil user >= today
+
+-- | Give a user premium for a set of days.
+addPremium :: UserId -> Integer -> Handler ()
+addPremium uid days = do
+  user <- fmap fromJust . runDB $ get uid
+  prem <- liftIO $ hasPremium user
+  today <- liftIO $ fmap utctDay getCurrentTime
+  let old = userPremiumUntil user
+  if prem
+    then runDB $ update uid [ UserPremiumUntil =. addDays days old ]
+    else runDB $ update uid [ UserPremium =. True,
+                              UserPremiumUntil =. addDays days today ]
 
