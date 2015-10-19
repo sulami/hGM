eb28ce7558a4527ad27c3702e1c36f1086247a2e
Robin Schroer <sulami@peerwire.org>
2015-10-19 18:49:01 +0200

    mock up a premium purchase page

--- Handler/Premium.hs
+++ Handler/Premium.hs
@@ -5,10 +5,14 @@ import Import
 getPremiumR :: Handler Html
 getPremiumR = do
   Entity uid user <- requireAuth
+  let periods = [1,3,6,12,24] :: [Int]
   defaultLayout $ do
     setTitle "Premium"
     $(widgetFile "premium")
 
 postPremiumR :: Handler Html
 postPremiumR = error "Not yet implemented: postPremiumR"
+
+prize :: Int -> Int
+prize m = m * 5 - (m - 1)
 
