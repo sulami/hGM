b96501f794ac4168e8547f62ee0b50c72ba783cc
Robin Schroer <sulami@peerwire.org>
2015-10-20 02:32:01 +0200

    disable the new campaign button if limit reached

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -1,11 +1,14 @@
 module Handler.Home where
 
 import Import
+import Import.Premium (hasPremium)
 
 getHomeR :: Handler Html
 getHomeR = do
-  user <- requireAuthId
-  camps <- runDB $ selectList [CampaignOwnerId ==. user] [Asc CampaignName]
+  Entity uid user <- requireAuth
+  prem <- hasPremium user
+  camps <- runDB $ selectList [CampaignOwnerId ==. uid] [Asc CampaignName]
+  let newenabled = prem || null camps
   defaultLayout $ do
     setTitle "Home"
     $(widgetFile "homepage")
