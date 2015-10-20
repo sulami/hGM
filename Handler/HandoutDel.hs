d6c73af1a2a4851f8ab6afb1661a97fe1a9cb051
Robin Schroer <sulami@peerwire.org>
2015-10-20 05:41:19 +0200

    add handout deletion

--- Handler/HandoutDel.hs
+++ Handler/HandoutDel.hs
@@ -3,7 +3,16 @@ module Handler.HandoutDel where
 import Import
 
 getHandoutDelR :: HandoutId -> Handler Html
-getHandoutDelR handoutId = error "Not yet implemented: getHandoutDelR"
+getHandoutDelR handoutId = do
+  user <- requireAuthId
+  handout <- runDB $ get404 handoutId
+  camp <- runDB . get404 $ handoutCampaignId handout
+  if user /= campaignOwnerId camp
+    then do
+      setMessage "Permission denied."
+      defaultLayout $ $(widgetFile "error")
+    else do
+      runDB $ delete handoutId
+      setMessage "handout deleted."
+      redirect . EntriesR . EntryListR $ handoutCampaignId handout
 
-postHandoutDelR :: HandoutId -> Handler Html
-postHandoutDelR handoutId = error "Not yet implemented: postHandoutDelR"
