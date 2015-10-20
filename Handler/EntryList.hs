631c839a6fc31495bd9beaa53792079dc29dc384
Robin Schroer <sulami@peerwire.org>
2015-10-20 02:49:48 +0200

    mock out the handouts within the campaigns

--- Handler/EntryList.hs
+++ Handler/EntryList.hs
@@ -1,12 +1,15 @@
 module Handler.EntryList where
 
 import           Import
+import           Import.Premium (hasPremium)
 
 getEntryListR :: CampaignId -> Handler Html
 getEntryListR cid = do
-  user <- requireAuthId
+  Entity uid user <- requireAuth
   camp <- runDB $ get404 cid
   entries <- runDB $ selectList [EntryCampaignId ==. cid] [Asc EntryName]
+  handouts <- runDB $ selectList [HandoutCampaignId ==. cid] [Asc HandoutName]
+  prem <- hasPremium user
   defaultLayout $ do
     setTitle . toHtml $ campaignName camp
     $(widgetFile "entrylist")
