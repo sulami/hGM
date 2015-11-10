f87ace3d6b766bf59920ae34aea640df1a04bdc5
Robin Schroer <sulami@peerwire.org>
2015-11-10 14:27:34 +0100

    prepare the page that holds the payment

--- Handler/Purchase.hs
+++ Handler/Purchase.hs
@@ -1,9 +1,19 @@
 module Handler.Purchase where
 
 import Import
+import Import.Premium (prizes)
+import Secret (stripeSecretKey)
 
 getPurchaseR :: Int -> Handler Html
-getPurchaseR = error "Not yet implemented: getPurchaseR"
+getPurchaseR months = do
+  let p = lookup months prizes
+  case p of
+    Nothing    -> error "Invalid parameter"
+    Just prize -> do
+      let sk = stripeSecretKey
+      defaultLayout $ do
+        setTitle "Get Premium"
+        $(widgetFile "purchase")
 
 postPurchaseR :: Int -> Handler Html
 postPurchaseR = error "Not yet implemented: postPurchaseR"
