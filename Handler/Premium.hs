504b3beccdbcc98c12c6d2e01f01241c57a2f0d4
Robin Schroer <sulami@peerwire.org>
2015-11-10 17:11:00 +0100

    add the stripe checkout dialogues

--- Handler/Premium.hs
+++ Handler/Premium.hs
@@ -2,15 +2,22 @@ module Handler.Premium where
 
 import Import
 import Import.Premium (prizes)
+import Secret
 
 getPremiumR :: Handler Html
 getPremiumR = do
-  _ <- requireAuth
+  Entity uid user <- requireAuth
   let periods = prizes
+      pubKey = stripePublicKey
+      userAddr = userIdent user
   defaultLayout $ do
     setTitle "Premium"
     $(widgetFile "premium")
 
 postPremiumR :: Handler Html
 postPremiumR = error "Not yet implemented: postPremiumR"
+
+-- | Convert a float prize to an in-cents int
+stripePrize :: Float -> Int
+stripePrize = round . (* 100)
 
