6e8a968838d77441a1df52bbca83a6243bc171a1
Robin Schroer <sulami@peerwire.org>
2015-11-10 17:11:57 +0100

    Revert "prepare the page that holds the payment"
    This reverts commit f87ace3d6b766bf59920ae34aea640df1a04bdc5.

--- Handler/Purchase.hs
+++ Handler/Purchase.hs
@@ -1,19 +1,9 @@
 module Handler.Purchase where
 
 import Import
-import Import.Premium (prizes)
-import Secret (stripeSecretKey)
 
 getPurchaseR :: Int -> Handler Html
-getPurchaseR months = do
-  let p = lookup months prizes
-  case p of
-    Nothing    -> error "Invalid parameter"
-    Just prize -> do
-      let sk = stripeSecretKey
-      defaultLayout $ do
-        setTitle "Get Premium"
-        $(widgetFile "purchase")
+getPurchaseR = error "Not yet implemented: getPurchaseR"
 
 postPurchaseR :: Int -> Handler Html
 postPurchaseR = error "Not yet implemented: postPurchaseR"
