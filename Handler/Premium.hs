2bdd27ac90992c68e7e4e6ee3d9642d555d550f1
Robin Schroer <sulami@peerwire.org>
2015-11-10 23:06:40 +0100

    add lots of checks to premium purchases and actually carry them out

--- Handler/Premium.hs
+++ Handler/Premium.hs
@@ -1,13 +1,13 @@
 module Handler.Premium where
 
-import Text.Read (read)
+import Text.Read (readMaybe)
 
 import Web.Stripe (stripe)
 import Web.Stripe.Charge (Currency (USD), TokenId (..), chargeCardByToken)
 
 import Import
-import Import.Premium (prizes)
-import Secret
+import Import.Premium (prizes, addPremium)
+import Secret (stripePublicKey, stripeSecretKey)
 
 getPremiumR :: Handler Html
 getPremiumR = do
@@ -21,30 +21,42 @@ getPremiumR = do
 
 postPremiumR :: Handler Html
 postPremiumR = do
-  Entity uid user <- requireAuth
+  Entity uid _ <- requireAuth
   token <- lookupPostParam "stripeToken"
   case token of
     Nothing  -> error "No token supplied"
     Just tok -> do
-      amount <- lookupPostParam "amount"
-      case amount of
-        Nothing  -> error "No amount supplied"
-        Just amt -> do
-          let secKey = stripeSecretKey
-          result <- liftIO $ stripe secKey $
-            chargeCardByToken (TokenId tok) USD (read $ unpack amt) Nothing
-          case result of
-            Left stripeError -> error $ show stripeError
-            Right charge     -> do
-              setMessage "Success"
-              redirect HomeR
-
--- TODO
--- Verify the amount matches the months
--- Give premium to the user
--- Add a proper success page
+      time <- parseParam "time"
+      case time of
+        Nothing -> error "Error passing the desired time"
+        Just tm -> do
+          let prize = lookup tm prizes
+          case prize of
+            Nothing -> error "Desired time is not available"
+            Just pz -> purchasePremium uid (TokenId tok) tm (stripePrize pz)
 
 -- | Convert a float prize to an in-cents int
 stripePrize :: Float -> Int
 stripePrize = round . (* 100)
+
+-- | Attempt to read a POST parameter
+parseParam :: Read a => Text -> Handler (Maybe a)
+parseParam name = do
+  val <- lookupPostParam name
+  case val of
+    Nothing -> return Nothing
+    Just v  -> return . readMaybe $ unpack v
+
+-- | Carry out the actual Premium purchase
+purchasePremium :: UserId -> TokenId -> Int -> Int -> Handler Html
+purchasePremium uid token time amount = do
+  let secKey = stripeSecretKey
+  result <- liftIO $ stripe secKey $
+    chargeCardByToken token USD amount Nothing
+  case result of
+    Left stripeError -> error $ show stripeError
+    Right _          -> do
+      addPremium uid (toInteger $ time * 30)
+      setMessage "Success"
+      redirect $ AccountR OverviewR
 
