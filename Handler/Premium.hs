5fcce39c20b235b93a6a73f0d9d6d45a8372d3c2
Robin Schroer <sulami@peerwire.org>
2015-11-10 17:56:52 +0100

    start processing payments on the server side

--- Handler/Premium.hs
+++ Handler/Premium.hs
@@ -1,12 +1,17 @@
 module Handler.Premium where
 
+import Text.Read (read)
+
+import Web.Stripe (stripe)
+import Web.Stripe.Charge (Currency (USD), TokenId (..), chargeCardByToken)
+
 import Import
 import Import.Premium (prizes)
 import Secret
 
 getPremiumR :: Handler Html
 getPremiumR = do
-  Entity uid user <- requireAuth
+  Entity _ user <- requireAuth
   let periods = prizes
       pubKey = stripePublicKey
       userAddr = userIdent user
@@ -15,7 +20,29 @@ getPremiumR = do
     $(widgetFile "premium")
 
 postPremiumR :: Handler Html
-postPremiumR = error "Not yet implemented: postPremiumR"
+postPremiumR = do
+  Entity uid user <- requireAuth
+  token <- lookupPostParam "stripeToken"
+  case token of
+    Nothing  -> error "No token supplied"
+    Just tok -> do
+      amount <- lookupPostParam "amount"
+      case amount of
+        Nothing  -> error "No amount supplied"
+        Just amt -> do
+          let secKey = stripeSecretKey
+          result <- liftIO $ stripe secKey $
+            chargeCardByToken (TokenId tok) USD (read $ unpack amt) Nothing
+          case result of
+            Left stripeError -> error $ show stripeError
+            Right charge     -> do
+              setMessage "Success"
+              redirect HomeR
+
+-- TODO
+-- Verify the amount matches the months
+-- Give premium to the user
+-- Add a proper success page
 
 -- | Convert a float prize to an in-cents int
 stripePrize :: Float -> Int
