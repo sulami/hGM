85a3a3a259ef6f10c65cae379e4a5149c90626e7
Robin Schroer <sulami@peerwire.org>
2015-12-01 00:00:50 +0100

    prepare the campaign options form

--- Handler/CampaignOpts.hs
+++ Handler/CampaignOpts.hs
@@ -1,9 +1,29 @@
 module Handler.CampaignOpts where
 
-import Import
+import           Import
+import           Import.Semantic (renderSemantic)
+
+prepCampForm :: Campaign -> Form Campaign
+prepCampForm camp = renderSemantic $ Campaign
+  <$> areq textField "Name" (Just $ campaignName camp)
+  <*> pure (campaignOwnerId camp)
 
 getCampaignOptsR :: CampaignId -> Handler Html
-getCampaignOptsR campaignId = error "Not yet implemented: getCampaignOptsR"
+getCampaignOptsR campaignId = do
+  user <- requireAuthId
+  campaign <- runDB $ get404 campaignId
+  if user /= campaignOwnerId campaign
+    then do
+      setMessage "Permission denied."
+      defaultLayout $ $(widgetFile "error")
+    else do
+      categories <- runDB $ selectList [CategoryCampaignId ==. campaignId]
+                      [Asc CategoryName]
+      (campWidget, enctype) <- generateFormPost $ prepCampForm campaign
+      defaultLayout $ do
+        setTitle . toHtml $ campaignName campaign
+        $(widgetFile "campaignopts")
 
 postCampaignOptsR :: CampaignId -> Handler Html
 postCampaignOptsR campaignId = error "Not yet implemented: postCampaignOptsR"
+
