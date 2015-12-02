b0bfc7b705f9e189338ac31e66364f3a81bde9e0
Robin Schroer <sulami@peerwire.org>
2015-12-02 13:46:15 +0100

    add adding of new categories

--- Handler/CategoryNew.hs
+++ Handler/CategoryNew.hs
@@ -1,7 +1,22 @@
 module Handler.CategoryNew where
 
-import Import
+import           Import
 
 postCategoryNewR :: CampaignId -> Handler Html
-postCategoryNewR campaignId = error "Not yet implemented: postCategoryNewR"
+postCategoryNewR campaignId = do
+  user <- requireAuthId
+  camp <- runDB $ get404 campaignId
+  if user /= campaignOwnerId camp
+    then do
+      setMessage "Permission denied."
+      defaultLayout $ $(widgetFile "error")
+    else do
+      newCatName <- lookupPostParam "catname"
+      case newCatName of
+        Nothing   -> do
+          setMessage "Internal error."
+          defaultLayout $ $(widgetFile "error")
+        Just name -> do
+          _ <- runDB . insert $ Category name campaignId
+          redirect . EntriesR $ CampaignOptsR campaignId
 
