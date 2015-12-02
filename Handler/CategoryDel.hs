8f3b8e46edb7b52fc3a70503e1f5257138e2aec2
Robin Schroer <sulami@peerwire.org>
2015-12-02 12:58:46 +0100

    add deletion of categories and move affected entries to uncategorized

--- Handler/CategoryDel.hs
+++ Handler/CategoryDel.hs
@@ -1,6 +1,15 @@
 module Handler.CategoryDel where
 
-import Import
+import           Import
 
 getCategoryDelR :: CategoryId -> Handler Html
-getCategoryDelR categoryId = error "Not yet implemented: getCategoryDelR"
+getCategoryDelR categoryId = do
+  _ <- requireAuth
+  category <- runDB $ get404 categoryId
+  let campaignId = categoryCampaignId category
+  entries <- runDB $ selectList [ EntryCampaignId ==. campaignId,
+                                  EntryCategoryId ==. Just categoryId ] []
+  forM_ entries $ \(Entity k _) -> runDB $ update k [EntryCategoryId =. Nothing]
+  runDB $ delete categoryId
+  redirect . EntriesR $ CampaignOptsR campaignId
+
