aa58c06ec8f545cdc617238bf05b052944f4f018
Robin Schroer <sulami@peerwire.org>
2015-12-02 13:34:21 +0100

    verify ownership of a campaign when deleting categories

--- Handler/CategoryDel.hs
+++ Handler/CategoryDel.hs
@@ -4,12 +4,19 @@ import           Import
 
 getCategoryDelR :: CategoryId -> Handler Html
 getCategoryDelR categoryId = do
-  _ <- requireAuth
+  user <- requireAuthId
   category <- runDB $ get404 categoryId
   let campaignId = categoryCampaignId category
-  entries <- runDB $ selectList [ EntryCampaignId ==. campaignId,
-                                  EntryCategoryId ==. Just categoryId ] []
-  forM_ entries $ \(Entity k _) -> runDB $ update k [EntryCategoryId =. Nothing]
-  runDB $ delete categoryId
-  redirect . EntriesR $ CampaignOptsR campaignId
+  camp <- runDB $ get404 campaignId
+  if user /= campaignOwnerId camp
+    then do
+      setMessage "Permission denied."
+      defaultLayout $ $(widgetFile "error")
+    else do
+      entries <- runDB $ selectList [ EntryCampaignId ==. campaignId,
+                                      EntryCategoryId ==. Just categoryId ] []
+      forM_ entries $ \(Entity k _) ->
+        runDB $ update k [EntryCategoryId =. Nothing]
+      runDB $ delete categoryId
+      redirect . EntriesR $ CampaignOptsR campaignId
 
